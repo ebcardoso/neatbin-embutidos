17422fc820d914156040b9241a79160ed490ce79
Breno Cardoso <elisiobreno@gmail.com>
2016-05-22 14:51:06 -0300

    Código com requisição HTTP

--- neatbin/neatbin.ino
+++ neatbin/neatbin.ino
@@ -1,45 +1,64 @@
-//Programa: Conectando Sensor Ultrassonico HC-SR04 ao Arduino
-//Autor: FILIPEFLOP
-
-//Carrega a biblioteca do sensor ultrassonico
-#include "Ultrasonic.h"
-
-//carrega a biblioteca da telinha
-#include <Wire.h>
-#include "LiquidCrystal_I2C.h"
-// Inicializa o display no endereco 0x27
-LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE);
+#include <EtherCard.h>
+#include <Ultrasonic.h>
 
 //Define os pinos para o trigger e echo
 #define pino_trigger 4
 #define pino_echo 5
-
 //Inicializa o sensor nos pinos definidos acima
 Ultrasonic ultrasonic(pino_trigger, pino_echo);
 
-void setup()
-{
-  lcd.begin (16,2);
-  lcd.setBacklight(HIGH);
+static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+byte Ethernet::buffer[700];
+static uint32_t timer;
+char website[] = "10.0.0.101";
+char temp_sensor[150];
+//int an0 = A0;
+//int sensor = 0;
 
-  lcd.setCursor(0,0);
-    lcd.print("Sensor Distancia");
-  lcd.setCursor(0,1);
-    lcd.print("Lendo dados do sensor...");
+static void my_callback (byte status, word off, word len)
+{
+   Ethernet::buffer[off+len] = 0;    //mensagem do retorno do servidor
+   //Serial.print((const char*) Ethernet::buffer + off);  //mostra todo o retorno
+   Serial.print((const char*) Ethernet::buffer + off + 157); //mostra somente ok
 }
 
-void loop()
+void setup ()
 {
-  //Le as informacoes do sensor, em cm e pol
+  delay(1000);
+  Serial.begin(9600);
+  Serial.println("Obtendo IP:");
+  if (ether.begin(sizeof Ethernet::buffer, mymac,10) == 0)
+    Serial.println( "Falha ao acessar o controlador Ethernet");
+  if (!ether.dhcpSetup())
+    Serial.println( "Falha ao obter o IP");
+  ether.printIp("IP: ", ether.myip);
+  ether.printIp("Mascara: ", ether.netmask);
+  ether.printIp("Gatway: ", ether.gwip);
+  ether.printIp("Sevidor DNS: ", ether.dnsip);
+  //if (!ether.dnsLookup(website))     //IP do servidor por DNS
+    //Serial.println("Falha no DNS");
+  ether.parseIp(ether.hisip, "10.0.0.101"); //IP do servidor manual
+  ether.printIp("Servidor: ", ether.hisip);
+  Serial.println(" ");
+}
+
+void loop ()
+{
   float cmMsec, inMsec;
   long microsec = ultrasonic.timing();
   cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
   inMsec = ultrasonic.convert(microsec, Ultrasonic::IN);
-  //Exibe informacoes no serial monitor
-  lcd.setCursor(0,1);
-    lcd.print("Dist: ");
-    lcd.print(cmMsec);
-    lcd.print("cm      ");
-    //lcd.println(inMsec);
-  delay(1000);
+  
+  ether.packetLoop(ether.packetReceive());
+  if (millis() > timer)
+  {
+    timer = millis() + 10000;
+    
+    //Serial.println((int)cmMsec);
+    
+    ether.persistTcpConnection(true);
+    //sensor = analogRead(an0);
+    sprintf(temp_sensor, "?cod=1&med=%i", (int)cmMsec); 
+    ether.browseUrl(PSTR("http://10.0.0.101/neatbin/insert.php?cod=1&med=50"), temp_sensor, website, my_callback); 
+  }
 }
